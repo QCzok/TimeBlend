7e3386f827102e8db08d0f286f580260b1661493
opPaybackChallenge <129500266+opPaybackChallenge@users.noreply.github.com>
2023-07-24 11:17:03 +0200

    Initial Commit

---
+++ TimeBlend/TimeBlendApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeBlendApp.swift
+//  TimeBlend
+//
+//  Created by Payback on 24.07.23.
+//
+
+import SwiftUI
+
+@main
+struct TimeBlendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
